946cfb7ab9e3158df661a8c04ceca213f2ef378b
cheoljin408 <kcj409@naver.com>
2022-10-26 10:21:23 +0900

    chapter04 설계 품질과 트레이드오프
    - 04 자율적인 객체를 향해

---
+++ src/chapter04/booking_movie_system_refactor/Customer.java
@@ -0,0 +1,12 @@
+package chapter04.booking_movie_system_refactor;
+
+public class Customer {
+
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ src/chapter04/booking_movie_system_refactor/DiscountConditionType.java
@@ -0,0 +1,7 @@
+package chapter04.booking_movie_system_refactor;
+
+public enum DiscountConditionType {
+
+    SEQUENCE,
+    PERIOD
+}
